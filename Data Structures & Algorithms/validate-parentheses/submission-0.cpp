542ea220bb40c90721dca8078e0d9171c9e6136a
Vipassana Khandare <vkhandar@usc.edu>
2026-06-18 19:59:46 -0700

    Add: validate-parentheses - submission-0

---
+++ Data Structures & Algorithms/validate-parentheses/submission-0.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+
+        for(char c: s){
+            if(c == '(' || c == '{' || c == '['){
+                st.push(c);
+            } else{
+                if(st.empty()) return false;
+                if((st.top() == '(' && c == ')') ||
+                    (st.top() == '{' && c == '}') ||
+                    (st.top() == '[' && c == ']')){
+                        st.pop();
+                } 
+                else{
+                    return false;
+                }
+            }
+        }
+        return st.empty();
+        
+    }
+};
